1437b2c4da97a77d92a94a4516f8090b89fde3e7
Rafael Lima de Morais <rafaa99@gmail.com>
2022-05-05 20:51:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mentoria-csv-projeto-my-custom-album/',
   build: {
     chunkSizeWarningLimit: 1600
   }
