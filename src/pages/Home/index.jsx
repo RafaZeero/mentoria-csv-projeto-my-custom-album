27168c7fb368512f09c4ce65a9da19fd76deb13b
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-13 19:48:20 -0300

    Tailwind- home part 1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,19 +3,38 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col gap-16 ">
-        <h1 className="text-5xl font-semibold">Album Personalizado</h1>
-        <div className="flex flex-col sm:flex-row ">
-          <Link to="games">
-            <img src="/assets/images/game-controller.svg" alt="Controller" />
+      <div className="flex flex-col gap-16 p-4 box-border">
+        <h1 className="text-4xl sm:text-5xl font-semibold ">
+          Album Personalizado
+        </h1>
+        <div className="flex flex-col sm:flex-row justify-center gap-16 p-4 ">
+          <Link
+            to="games"
+            className="border-2 border-black rounded-xl shadow-customShadow hover:scale-[1.03] duration-200"
+          >
+            <img
+              src="/assets/images/game-controller.svg"
+              alt="Controller"
+              className="p-4"
+            />
           </Link>
 
-          <Link to="movies">
-            <img src="/assets/images/movie-camera.svg" alt="Camera" />
+          <Link
+            to="movies"
+            className="border-2 border-black rounded-xl shadow-customShadow hover:scale-[1.03] duration-200"
+          >
+            <img
+              src="/assets/images/movie-camera.svg"
+              alt="Camera"
+              className="p-4"
+            />
           </Link>
 
-          <Link to="books">
-            <img src="/assets/images/books.svg" alt="Books" />
+          <Link
+            to="books"
+            className="border-2 border-black rounded-xl shadow-customShadow hover:scale-[1.03] duration-200"
+          >
+            <img src="/assets/images/books.svg" alt="Books" className="p-4" />
           </Link>
         </div>
         <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     extend: {},
     family: {
       Roboto: ['Roboto', 'sans-serif']
+    },
+    boxShadow: {
+      customShadow:
+        'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px'
     }
   },
   plugins: []
